0fc3cb50070af269e46d75899b214e8f7ee51d94
Racim Saal <racimsaal@gmail.com>
2023-07-11 20:39:03 +0100

    testing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,19 @@ import {
   ForbiddenException,
   Injectable,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     // generate password hash
     const hash = await argon.hash(dto.password);
@@ -20,9 +26,8 @@ export class AuthService {
           hashedPassword: hash,
         },
       });
-      delete user.hashedPassword;
       // return saved user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -60,7 +65,25 @@ export class AuthService {
     }
 
     //send back the user
-    delete user.hashedPassword;
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ acces_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return {
+      acces_token: token,
+    };
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Get,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('user')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser() {}
+}
